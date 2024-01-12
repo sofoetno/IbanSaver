58773d7662be5677e00bfc056d73339417075ff6
Jamaტa <jamata94@gmail.com>
2024-01-12 11:25:48 +0400

    add Custom TextField style
    add CustomTextFieldWithIcon View

---
+++ IbanSaver/Components/CustomTextFieldStyleView.swift
@@ -0,0 +1,31 @@
+//
+//  CustomTextFieldStyleView.swift
+//  IbanSaver
+//
+//  Created by Nika Jamatashvili on 12.01.24.
+//
+
+import SwiftUI
+
+struct CustomTextFieldWithIcon: View {
+    @Binding var text: String
+    var placeholder: String
+    var iconName: String
+
+    var body: some View {
+        HStack {
+            TextField(placeholder, text: $text)
+            
+            Image(systemName: iconName)
+                .foregroundColor(AppColors.darkGray)
+                .font(.system(size: 24))
+                .padding(.leading, 12)
+        }
+        .frame(maxWidth: .infinity)
+        .frame(height: 56)
+        .foregroundColor(AppColors.black)
+        .padding(.horizontal, 16)
+        .background(AppColors.silver)
+        .cornerRadius(16)
+    }
+}
